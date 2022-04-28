a967e6cf27da1290bd24f2b0d1cc2660a1ed5e9c
Tanjir Ahmed <tanjira584@gmail.com>
2022-04-29 02:30:52 +0600

    Connected With Mongodb

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -19,6 +19,18 @@ async function run() {
     try {
         await client.connect();
         const motorDelar = client.db("motorStore").collection("motor");
+        app.get("/motors", async (req, res) => {
+            const query = {};
+            const cursor = motorDelar.find(query);
+            const motors = await cursor.toArray();
+            res.send(motors);
+        });
+        app.get("/motor/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await motorDelar.findOne(query);
+            res.send(result);
+        });
     } finally {
     }
 }
