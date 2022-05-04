f9ebad53e303b12f1bfe4a6d09f198cd2aa16345
Tanjir Ahmed <tanjira584@gmail.com>
2022-05-04 21:22:05 +0600

    JWT implement

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
+const jwt = require("jsonwebtoken");
 
 //Middleware
 app.use(cors());
@@ -30,7 +31,12 @@ async function run() {
             const result = await motorDelar.insertOne(newMotor);
             res.send(result);
         });
-
+        app.get("/motor/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await motorDelar.findOne(query);
+            res.send(result);
+        });
         app.delete("/motor/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -60,10 +66,45 @@ async function run() {
             );
             res.send(result);
         });
+        app.post("/login", async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                expiresIn: "1d",
+            });
+            res.send({ accessToken });
+        });
+        app.get("/my-items", verifyJwt, async (req, res) => {
+            const decodeEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodeEmail) {
+                const query = { dEmail: email };
+                const cursor = motorDelar.find(query);
+                const motors = await cursor.toArray();
+                res.send(motors);
+            } else {
+                res.status(403).send({ message: "Forbiden Access" });
+            }
+        });
     } finally {
     }
 }
 run().catch(console.dir);
+
+function verifyJwt(req, res, next) {
+    const bearToken = req.headers.authorization;
+    if (!bearToken) {
+        return res.status(401).send({ message: "Unauthorize access" });
+    }
+    const token = bearToken.split(" ")[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbiden Access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 app.get("/", async (req, res) => {
     res.send("Hello , This is home route of server");
